5af3f90b1eec34fec6c2a7e6996a9f9f695f8c6c
Nick Marriotti <nmarriotti@gmail.com>
2017-03-13 21:53:53 -0700

    Updated some comments for better readability

--- ClassTutorial/classes/person.py
+++ ClassTutorial/classes/person.py
@@ -1,7 +1,11 @@
+# The __init__.py file inside this folder tells Python a file can be imported from this directory
+
 class Person():
 
+    # This __init__ AKA initialize method is called everytime a person object is created
     def __init__(self, name):
         self.name = name
 
+    # Returns the persons name
     def getName(self):
         return self.name
